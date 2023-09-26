b166f7caacf85cccdf0d6933f51a4146a722a09f
Sean Choi <seanchoi@adobe.com>
2023-09-25 19:44:55 -0600

    Update scripts.js

--- homepage/scripts/scripts.js
+++ homepage/scripts/scripts.js
@@ -9,7 +9,6 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-
 import { setLibs } from './utils.js';
 
 // Add project-wide style path here.
