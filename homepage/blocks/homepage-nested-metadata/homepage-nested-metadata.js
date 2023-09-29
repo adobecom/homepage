7cb87e97f440b036ad393fecbc8b11b85e8e9f2e
vivgoodrich <vivian.goodrich@gmail.com>
2023-09-29 15:27:46 -0600

    no variants needed in nested metadata

--- homepage/blocks/homepage-nested-metadata/homepage-nested-metadata.js
+++ homepage/blocks/homepage-nested-metadata/homepage-nested-metadata.js
@@ -1,25 +1,19 @@
 import { getLibs } from '../../scripts/utils.js';
 
 export default async function init(el) {
-  if (el.classList.contains('begin')) {
-    const section = el.parentNode;
-    const end = el.parentNode.querySelector(':scope > div.homepage-nested-metadata.end');
-    if (end) {
-      const children = section.children;
-      var childArray = Array.prototype.slice.call(children);
-      const beginIdx = childArray.indexOf(el);
-      const endIdx = childArray.indexOf(end);
-      if (endIdx > beginIdx) {
-        const miloLibs = getLibs();
-        const style = el.classList.contains('masonry') ? ' masonry masonry-up' : '';
-        const key = el.querySelector(':scope > div:nth-child(1) > div:nth-child(1)')?.textContent;
-        const { createTag } = await import(`${miloLibs}/utils/utils.js`);
-        const nested = createTag('div', { class: `section${style}` }, false);
-        for (let i = beginIdx + 1; i < endIdx; i++) nested.appendChild(childArray[i]);
-        section.insertBefore(nested, el);
-        end.remove();
-        el.remove();
-      }
-    }
+  const section = el.parentNode;
+  const nestedMetadata = el.parentNode.querySelectorAll(':scope > div.homepage-nested-metadata');
+  if (nestedMetadata.length >= 2 && el === nestedMetadata[0]) {
+    const children = section.children;
+    var childArray = Array.prototype.slice.call(children);
+    const beginIdx = childArray.indexOf(nestedMetadata[0]);
+    const endIdx = childArray.indexOf(nestedMetadata[1]);
+      const miloLibs = getLibs();
+      const { createTag } = await import(`${miloLibs}/utils/utils.js`);
+      const nestedSection = createTag('div', { class: `section masonry masonry-up` }, false);
+      for (let i = beginIdx + 1; i < endIdx; i++) nestedSection.appendChild(childArray[i]);
+      section.insertBefore(nestedSection, nestedMetadata[0]);
+      nestedMetadata[0].remove();
+      nestedMetadata[1].remove();
   }
 }
